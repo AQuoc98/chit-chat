dbac5a640e3dba1ca31db08ba3d58e0dd46a3c77
AQuoc98 <quocnguyen.developer@gmail.com>
2026-02-13 11:26:11 +0700

    BE/feat(1.0): Update authMe controller to return authenticated user info

--- apps/chit-chat-be/src/controllers/user-controller.ts
+++ apps/chit-chat-be/src/controllers/user-controller.ts
@@ -1,5 +1,12 @@
 import { Request, Response } from "express";
 
 export const authMe = async (req: Request, res: Response) => {
-  return res.status(200).json({ message: "Authenticated user info." });
+  try {
+    const user = req.user;
+
+    return res.status(200).json({ user });
+  } catch (error) {
+    console.log("Error in authMe controller:", error);
+    return res.status(500).json({ message: "Internal server error." });
+  }
 };
