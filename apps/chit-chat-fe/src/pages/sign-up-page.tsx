3d2c78b440004b2ad8ac9de83760319c06c620bc
AQuoc98 <quocnguyen.developer@gmail.com>
2026-02-20 12:54:34 +0700

    FE/feat(1.0): Add Sign Up page

--- apps/chit-chat-fe/src/pages/sign-up-page.tsx
+++ apps/chit-chat-fe/src/pages/sign-up-page.tsx
@@ -1,7 +1,11 @@
+import { SignupForm } from "@/components/auth/signup-form";
+
 const SignUpPage = () => {
   return (
-    <div className="flex h-screen items-center justify-center">
-      <h1 className="text-3xl font-bold">Sign Up Page</h1>
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10 absolute inset-0 z-0 bg-gradient-purple">
+      <div className="w-full max-w-sm md:max-w-4xl">
+        <SignupForm />
+      </div>
     </div>
   );
 };
---
+++ apps/chit-chat-fe/src/stores/use-auth-store.ts
@@ -0,0 +1,31 @@
+import { create } from "zustand";
+import { toast } from "sonner";
+import type { AuthState } from "@/types/store";
+import { authService } from "@/services/auth-service";
+
+export const useAuthStore = create<AuthState>((set, get) => ({
+  accessToken: null,
+  user: null,
+  loading: false,
+  signUp: async (username, password, email, firstName, lastName) => {
+    try {
+      set({ loading: true });
+
+      await authService.signUp(username, password, email, firstName, lastName);
+
+      toast.success(
+        "Đăng ký thành công! Bạn sẽ được chuyển sang trang đăng nhập.",
+      );
+
+      return true;
+    } catch (error) {
+      console.error(error);
+
+      toast.error("Đăng ký không thành công");
+
+      return false;
+    } finally {
+      set({ loading: false });
+    }
+  },
+}));
